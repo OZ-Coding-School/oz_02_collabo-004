2c3580d53d4650924f98291bea8899f53dcf3501
FLOWOLFOX <flowolfox@gmail.com>
2024-05-02 10:07:14 +0900

    feat: 저장한 북스포일러 컴포넌트 스타일 수정

--- src/components/MyBookSpoiler/index.jsx
+++ src/components/MyBookSpoiler/index.jsx
@@ -1,10 +1,10 @@
 const MyBookSpoiler = () => {
   return (
-    <div className="flex items-end justify-between w-[1000px] h-[180px]">
+    <div className="flex items-end justify-between w-[850px] h-[180px]">
       <div className="flex items-center justify-center w-[120px] h-[150px] bg-gray-500 text-white font-bold">
         책 이미지
       </div>
-      <div className="flex flex-col justify-center rounded-3xl w-[860px] h-[130px] bg-[#fcf2c9] pl-6">
+      <div className="flex flex-col justify-center rounded-3xl w-[710px] h-[130px] bg-[#fcf2c9] pl-6">
         <span className="text-xl font-bold">
           당신은 사업가입니까 | 캐럴 로스 저
         </span>
