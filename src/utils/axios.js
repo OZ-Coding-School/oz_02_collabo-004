8b1227bcdaa693b133b3a3a14007a4bc0649f3f2
h220101 <ssw3417@naver.com>
2024-05-21 19:19:36 +0900

    Refactor: viteconfig-usefetch-axios-bookspoilerdetail-수정

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://ec2-13-209-68-37.ap-northeast-2.compute.amazonaws.com:8000',
-  // baseURL: import.meta.env.VITE_BASE_URL,
-  baseURL: "/api",
-  withCredentials: true,
+  baseURL: 'http://ec2-13-209-68-37.ap-northeast-2.compute.amazonaws.com:8000',
+  //baseURL: import.meta.env.VITE_BASE_URL,
+  //baseURL: "/api",
+  changeOrigin: true,
+  secure: false,
+  ws: true,
 });
 
 export default axiosInstance;
