570f97b1b40872ddc7af5eb0e1bef31cb95322f6
h220101 <ssw3417@naver.com>
2024-05-21 23:10:11 +0900

    Feat: proxy 설정

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,6 +4,7 @@ const axiosInstance = axios.create({
   baseURL: 'http://ec2-13-209-68-37.ap-northeast-2.compute.amazonaws.com:8000',
   //baseURL: import.meta.env.VITE_BASE_URL,
   //baseURL: "/api",
+  withCredentials: true,
   changeOrigin: true,
   secure: false,
   ws: true,
