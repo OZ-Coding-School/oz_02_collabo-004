d787e56b9f179ab7a24702c894847ca391fef1ec
togom <jkisung1@naver.com>
2024-05-23 12:08:15 +0900

    Feat: main 수정 배포2

--- src/components/BookCaroussel/BookCarrousel.jsx
+++ src/components/BookCaroussel/BookCarrousel.jsx
@@ -1,13 +1,11 @@
-
-import React, { useState } from "react";
-import BookCardKeyword from "../BookInfoCard/BookCardKeyword";
-import { Link, useLocation } from "react-router-dom";
-import Button from "../@common/Button";
-import leftArrow from "../../assets/images/challenge_left_arrow.png";
-import rightArrow from "../../assets/images/challenge_right_arrow.png";
-import useFetch from "../../hooks/useFetch";
-import { BOOK_DATA } from "../../components/BookInfoCard/data";
-
+import React, { useState } from 'react';
+import BookCardKeyword from '../BookInfoCard/BookCardKeyword';
+import { Link, useLocation } from 'react-router-dom';
+import Button from '../@common/Button';
+import leftArrow from '../../assets/images/challenge_left_arrow.png';
+import rightArrow from '../../assets/images/challenge_right_arrow.png';
+import useFetch from '../../hooks/useFetch';
+import { BOOK_DATA } from '../../components/BookInfoCard/data';
 
 const BookCaroussel = ({
   bookId,
@@ -20,7 +18,10 @@ const BookCaroussel = ({
   //   ok: true,
   // };
   // response.data = BOOK_DATA;
-  // const { data: book } = useFetch("http://ec2-13-209-68-37.ap-northeast-2.compute.amazonaws.com//books", response);
+  // const { data: book } = useFetch(
+  //   'http://ec2-13-209-68-37.ap-northeast-2.compute.amazonaws.com//books',
+  //   response
+  // );
 
   const location = useLocation();
   const [counter, setCounter] = useState(0);
@@ -50,53 +51,56 @@ const BookCaroussel = ({
       <div className="flex relative z-10 w-[936px] overflow-hidden">
         {BOOK_DATA &&
           BOOK_DATA.map((book) => (
-            <div
-              key={bookId}
-              className="h-[520px] w-[280px] flex justify-center items-center z-10 mx-4 transition-all ease-in-out duration-500"
-              style={{
-                transform: `translateX(-${936 * counter}px)`,
-              }}
-            >
-              <div className="z-20">
-
-                {location.pathname === "/challenge" ? (
-                  <div className="bg-primary w-[280px] h-[400px] absolute top-[160px] z-20"></div>
-                ) : (
-                  <div className="bg-primary w-[280px] h-[340px] absolute top-[160px] z-20"></div>
-                )}
-              </div>
-              <div className="flex flex-col relative z-30 items-center gap-4">
-                <div className="z-30 border">
-                  <img src={book.book_img} className=" w-[200px]" alt="book" />
+            <>
+              <div
+                key={book.Id}
+                className="h-[520px] w-[280px] flex justify-center items-center z-10 mx-4 transition-all ease-in-out duration-500"
+                style={{
+                  transform: `translateX(-${936 * counter}px)`,
+                }}
+              >
+                <div className="z-20">
+                  {location.pathname === '/challenge' ? (
+                    <div className="bg-primary w-[280px] h-[400px] absolute top-[160px] z-20"></div>
+                  ) : (
+                    <div className="bg-primary w-[280px] h-[340px] absolute top-[160px] z-20"></div>
+                  )}
                 </div>
-                <div key={book.id} className="w-[280px] flex flex-col ">
-                  <p className="font-black text-2xl text-center z-30">
-                    {book.name}
-                  </p>
-                </div>
-                <div
-                  key={book.id}
-                  className="flex justify-evenly w-[270px] z-30"
-                >
-                  <p className="font-bold">{book.author}</p>
-                  <p className="font-bold text-gray8">{book.published}</p>
-                </div>
-                {/* <div className="flex flex-row justify-evenly w-[230px] z-30">
-                  <BookCardKeyword></BookCardKeyword> */}
-                <div className="flex gap-2 justify-center w-[230px] z-30">
-                  <BookCardKeyword />
-                </div>
-                {location.pathname === '/challenge' ? (
-                  <div className="w-[240px] z-30 flex justify-center">
-                    <Link to={isLoggedIn ? '/payment' : '/login'}>
-                      <Button>챌린지용 북스포일러 열람하기</Button>
-                    </Link>
+                <div className="flex flex-col relative z-30 items-center gap-4">
+                  <div className="z-30 border">
+                    <img
+                      src={book.book_img}
+                      className=" w-[200px]"
+                      alt="book"
+                    />
                   </div>
-                ) : (
-                  ''
-                )}
+                  <div key={book.id} className="w-[280px] flex flex-col ">
+                    <p className="font-black text-2xl text-center z-30">
+                      {book.name}
+                    </p>
+                  </div>
+                  <div
+                    key={book.id}
+                    className="flex justify-evenly w-[270px] z-30"
+                  >
+                    <p className="font-bold">{book.author}</p>
+                    <p className="font-bold text-gray8">{book.published}</p>
+                  </div>
+                  <div className="flex gap-2 justify-center w-[230px] z-30">
+                    <BookCardKeyword />
+                  </div>
+                  {location.pathname === '/challenge' ? (
+                    <div className="w-[240px] z-30 flex justify-center">
+                      <Link to={isLoggedIn ? '/payment' : '/login'}>
+                        <Button>챌린지용 북스포일러 열람하기</Button>
+                      </Link>
+                    </div>
+                  ) : (
+                    ''
+                  )}
+                </div>
               </div>
-            </div>
+            </>
           ))}
       </div>
       <div>
