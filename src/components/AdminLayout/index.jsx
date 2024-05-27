23ff4c9ac7ea4c7a1e87808e8aa21d5300dbc1b2
togom <jkisung1@naver.com>
2024-05-27 18:17:11 +0900

    Feat: 어떤 기능 어쩌구

--- src/components/AdminLayout/index.jsx
+++ src/components/AdminLayout/index.jsx
@@ -7,14 +7,14 @@ const AdminLayout = () => {
   return (
     <>
       <AdminHeader />
-        <div className="flex justify-center">
-          <div className="md flex justify-between pt-[120px] pb-[80px]">
-            <div className='h-full'>
-              <AdminSidebar />
-            </div>
-            <Outlet />
+      <div className="flex justify-center">
+        <div className="md flex justify-between pt-[120px] pb-[80px]">
+          <div className="h-full">
+            <AdminSidebar />
           </div>
+          <Outlet />
         </div>
+      </div>
       <Footer />
     </>
   );
